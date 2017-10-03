4d2c5c2de3e5371b397b15139ba582fad7887fc2
Adam Bouzourene <adam1@172-16-27-193.DYNAPOOL.NYU.EDU>
2017-10-03 11:50:22 -0400

    error handling, html templating

--- app.go
+++ app.go
@@ -4,41 +4,62 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"html/template"
 )
 
 type Page struct {
-    Title string
-    Body  []byte
+	Title string
+	Body  []byte
 }
 
 //This is a method named save that takes as its receiver p, a pointer to Page. It takes no parameters, and returns a value of type error
 func (p *Page) save() error {
-    filename := p.Title + ".txt"
-    return ioutil.WriteFile(filename, p.Body, 0600)
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
 // constructs the file name from the title parameter, reads the file's contents into a new variable body, and returns a pointer to a Page literal constructed with the proper title and body values.
 func loadPage(title string) (*Page, error) {
-    filename := title + ".txt"
-    body, err := ioutil.ReadFile(filename)
-    if err != nil {
-        return nil, err
-    }
-    return &Page{Title: title, Body: body}, nil
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
 }
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-  	title := r.URL.Path[len("/view/"):]
-  	p, _ := loadPage(title)
-  	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
-  }
+    title := r.URL.Path[len("/view/"):]
+    p, err := loadPage(title)
+    if err != nil {
+        http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+        return
+    }
+    renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+    title := r.URL.Path[len("/edit/"):]
+    p, err := loadPage(title)
+    if err != nil {
+        p = &Page{Title: title}
+    }
+    renderTemplate(w, "edit", p)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+    fmt.Printf("render file: %s.html\n", tmpl)
+    t, _ := template.ParseFiles(tmpl + ".html")
+    t.Execute(w, p)
+}
 
 func main() {
 	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-    p1.save()
-    p2, _ := loadPage("TestPage")
-    fmt.Println(string(p2.Body))
+	p1.save()
+	p2, _ := loadPage("TestPage")
+	fmt.Println(string(p2.Body))
 
-    http.HandleFunc("/view/", viewHandler)
-    fmt.Println(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	fmt.Println(http.ListenAndServe(":8080", nil))
 }
