7a556b3c176a8ad5165c40fa63a15ffb3f0fdb26
Adam Bouzourene <adam1@172-16-27-193.DYNAPOOL.NYU.EDU>
2017-10-03 11:07:31 -0400

    basic web server and file structures

--- app.go
+++ app.go
@@ -1,9 +1,41 @@
 package main
 
 import (
-
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
-func main() {
+type Page struct {
+    Title string
+    Body  []byte
+}
 
+//This is a method named save that takes as its receiver p, a pointer to Page. It takes no parameters, and returns a value of type error
+func (p *Page) save() error {
+    filename := p.Title + ".txt"
+    return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+// constructs the file name from the title parameter, reads the file's contents into a new variable body, and returns a pointer to a Page literal constructed with the proper title and body values.
+func loadPage(title string) (*Page, error) {
+    filename := title + ".txt"
+    body, err := ioutil.ReadFile(filename)
+    if err != nil {
+        return nil, err
+    }
+    return &Page{Title: title, Body: body}, nil
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func main() {
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+    p1.save()
+    p2, _ := loadPage("TestPage")
+    fmt.Println(string(p2.Body))
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8080", nil)
 }
